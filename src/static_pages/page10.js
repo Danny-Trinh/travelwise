26065cf0005c14ca6049ae0fa6c114a718f9cb3e
amgluch <adammgluch@gmail.com>
2020-09-26 16:35:02 -0500

    Included flight with multiple legs, put single leg flight in page 9

--- src/static_pages/page10.js
+++ src/static_pages/page10.js
@@ -7,6 +7,7 @@ export default class page10 extends Component {
         <table class="table">
           <thead class="thead-dark">
             <tr>
+              <th scope="col">Leg</th>
               <th scope="col">Departing From</th>
               <th scope="col">Departure Terminal</th>
               <th scope="col">Destination</th>
@@ -20,18 +21,32 @@ export default class page10 extends Component {
           </thead>
           <tbody>
             <tr>
-              <th scope="row">Dallas</th>
-              <td>E</td>
-              <td>JFK</td>
-              <td>4</td>
-              <td>Round-trip</td>
-              <td>46.53</td>
-              <td>9</td>
-              <td>2020-10-03T18:05:00</td>
-              <td>2020-10-04T00:05:00</td>
+              <th scope="row">1</th>
+	              <td>Dallas</td>
+	              <td>E</td>
+	              <td>ATL</td>
+	              <td>S</td>
+	              <td>Round-trip</td>
+	              <td>46.53</td>
+	              <td>9</td>
+	              <td>2020-10-03T18:05:00</td>
+	              <td>2020-10-03T21:12:00</td>
+	        </tr>
+	        <tr>
+		          <th scope="row">2</th>
+		          <td>ATL</td>
+		          <td>S</td>
+		          <td>JFK</td>
+		          <td>4</td>
+		          <td>Round-trip</td>
+		          <td>46.53</td>
+		          <td>9</td>
+		          <td>2020-10-03T21:52:00</td>
+		          <td>2020-10-04T00:05:00</td>
             </tr>
           </tbody>
         </table>
+
       </div>
     );
   }
