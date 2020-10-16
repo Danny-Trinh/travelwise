6dd50d9afbd52d34e23de29a298acda905fc73f0
amgluch <adammgluch@gmail.com>
2020-10-15 22:24:33 -0500

    attempted backend unit test fix

--- travelwiseBackend/TravelwiseBackendUnitTests.py
+++ travelwiseBackend/TravelwiseBackendUnitTests.py
@@ -1,13 +1,9 @@
 import unittest
-from application import appA
 
 class DummyTests(unittest.TestCase):
 
     def test_one_eq_one(self):
         self.assertEqual(1,1)
-    
-    def test_import_from_app(self):
-        self.assertEqual(10, appA)
 
 if __name__ == '__main__':
     unittest.main()
