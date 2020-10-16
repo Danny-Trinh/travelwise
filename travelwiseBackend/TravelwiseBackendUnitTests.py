04188022e1afb56f441bbc74c663df0adff03b94
amgluch <adammgluch@gmail.com>
2020-10-15 23:04:16 -0500

    removed bandaid fix

--- travelwiseBackend/TravelwiseBackendUnitTests.py
+++ travelwiseBackend/TravelwiseBackendUnitTests.py
@@ -11,17 +11,17 @@ class DummyTests(unittest.TestCase):
     def test_two_cities(self):
         response = requests.get(base_url + "cities").json()
         first_city = str(response[0]['name'])
-        self.assertEqual("[u'Quezon City']", first_city)
+        self.assertEqual("['Quezon City']", first_city)
 
     def test_three_aiport(self):
         response = requests.get(base_url + "airport").json()
         first_airport = str(response[0]['airport_name'])
-        self.assertEqual("[u'SUVARNABHUMI INTL']", first_airport)
+        self.assertEqual("['SUVARNABHUMI INTL']", first_airport)
 
     def test_four_covid(self):
         response = requests.get(base_url + "covid").json()
         first_covid = str(response[0]['country'])
-        self.assertEqual("[u'Afghanistan']", first_covid)
+        self.assertEqual("['Afghanistan']", first_covid)
 
 
 if __name__ == '__main__':
