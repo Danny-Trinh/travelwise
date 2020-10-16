bf6087dfe29ac5e5e86cff673333429e1b6f7e79
amgluch <adammgluch@gmail.com>
2020-10-15 23:01:38 -0500

    there's no going back

--- travelwiseBackend/TravelwiseBackendUnitTests.py
+++ travelwiseBackend/TravelwiseBackendUnitTests.py
@@ -1,7 +1,6 @@
 import unittest
 import requests
 
-
 base_url = "http://travelwisebackend.us-east-2.elasticbeanstalk.com/" 
 
 class DummyTests(unittest.TestCase):
@@ -12,20 +11,17 @@ class DummyTests(unittest.TestCase):
     def test_two_cities(self):
         response = requests.get(base_url + "cities").json()
         first_city = str(response[0]['name'])
-        first_city = first_city[2:len(first_city) - 2]
-        self.assertEqual("Quezon City", first_city)
+        self.assertEqual("[u'Quezon City']", first_city)
 
     def test_three_aiport(self):
         response = requests.get(base_url + "airport").json()
         first_airport = str(response[0]['airport_name'])
-        first_airport = first_airport[2:len(first_airport) - 2]
-        self.assertEqual("SUVARNABHUMI INTL", first_airport)
+        self.assertEqual("[u'SUVARNABHUMI INTL']", first_airport)
 
     def test_four_covid(self):
         response = requests.get(base_url + "covid").json()
         first_covid = str(response[0]['country'])
-        first_covid = first_covid[2:len(first_covid) - 2]
-        self.assertEqual("Afghanistan", first_covid)
+        self.assertEqual("[u'Afghanistan']", first_covid)
 
 
 if __name__ == '__main__':
