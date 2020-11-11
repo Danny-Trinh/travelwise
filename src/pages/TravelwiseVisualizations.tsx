924e9e22fb4434f8552d47c3bef23e80d33b7400
maximuschu <maximuschu@utexas.edu>
2020-11-11 01:45:15 -0600

    Testing d3

--- src/pages/TravelwiseVisualizations.tsx
+++ src/pages/TravelwiseVisualizations.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BarChart from './Chart1';
 
 export default class TravelwiseVisualizations extends Component {
     render() {
@@ -7,6 +8,8 @@ export default class TravelwiseVisualizations extends Component {
             <div className="text-center">
                 <h1 className="container-fluid bg-gray-200 pt-4 m-0 pb-5">Travelwise Visualizations </h1>
             </div>
+            <div>Covid Cases:</div>
+            <div><BarChart /></div>
         </div>
       );
     }
