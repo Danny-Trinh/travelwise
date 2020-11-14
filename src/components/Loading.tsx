dfbd9ee54181530ff704526f1beb3e48bfd34ebd
Danny-Trinh <dtrinh403@yahoo.com>
2020-11-14 16:21:29 -0600

    made load animation

---
+++ src/components/Loading.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { motion } from "framer-motion";
+const bounce = 40;
+const staggerDelay = 0.2;
+
+const containerStyle = {
+  width: "20rem",
+  height: "4rem",
+  display: "flex",
+  justifyContent: "space-around",
+  margin: "5rem auto 5rem auto",
+};
+const circleStyle = {
+  height: "4rem",
+  width: "4rem",
+  borderRadius: "50%",
+  backgroundColor: "#319795",
+};
+const itemVariants = {
+  animate: {
+    y: [bounce, -bounce, bounce],
+    transition: {
+      ease: "easeInOut",
+      duration: 2,
+      loop: Infinity,
+    },
+  },
+};
+
+const containerVariants = {
+  animate: {
+    transition: {
+      staggerChildren: staggerDelay,
+    },
+  },
+};
+
+export default function Loading(props: any) {
+  return (
+    <div className="container">
+      <motion.div
+        style={containerStyle}
+        initial="hidden"
+        animate="animate"
+        variants={containerVariants}
+      >
+        <motion.div variants={itemVariants} style={circleStyle} />
+        <motion.div variants={itemVariants} style={circleStyle} />
+        <motion.div variants={itemVariants} style={circleStyle} />
+      </motion.div>
+    </div>
+  );
+}
