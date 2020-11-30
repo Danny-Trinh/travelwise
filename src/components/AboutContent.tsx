4aa8898113819f68ba924be3770d10052ae6f5ec
Danny-Trinh <dtrinh403@yahoo.com>
2020-11-30 17:22:50 -0600

    added presentation

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { apps, apiSources } from "../utility/data";
-import pdf from "../utility/TravelwisePresentation.pdf";
 import { FaGitlab, FaVideo, FaUserAlt } from "react-icons/fa";
 import { SiPostman } from "react-icons/si";
 
@@ -43,7 +42,7 @@ export default function AboutLinks(props: any) {
         <li className="mb-2">
           <FaGitlab className="mr-2 t-teal-700" />
           <a className="link" href="https://gitlab.com/Danny-Trinh/travelwise">
-            https://gitlab.com/Danny-Trinh/travelwise
+            Our Gitlab Repository
           </a>
         </li>
         <li className="mb-2">
@@ -52,13 +51,16 @@ export default function AboutLinks(props: any) {
             className="link"
             href="https://documenter.getpostman.com/view/12799472/TVev454C"
           >
-            https://documenter.getpostman.com/view/12799472/TVev454C
+            Our API Documentation
           </a>
         </li>
         <li className="mb-2">
           <FaUserAlt className="mr-2 t-teal-700" />
-          <a className="link" href={pdf}>
-            Our Presentation PDF
+          <a
+            className="link"
+            href="https://docs.google.com/presentation/d/1E4bDHqTQkFzSYBRukHFEKn5FgFhvqzTiQLyR93SxDH4/edit?usp=sharing"
+          >
+            Our Presentation
           </a>
         </li>
         <li className="mb-2">
